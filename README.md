1d73a72e8db4c3e1031de2ba69b5d0d95259bc64
Maksim <th0r@inbox.ru>
2018-10-30 00:29:42 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# nuxt-tpl
---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template lang="pug">
+  section.container
+    div
+      h1.title ТИТЛЕ!
+</template>
+
+<script>
+import AppLogo from '~/components/AppLogo.vue'
+
+export default {
+  components: {
+    AppLogo
+  }
+}
+</script>
+
+<style lang="sass">
+.container
+  min-height: 100vh
+  display: flex
+  justify-content: center
+  align-items: center
+  text-align: center
+</style>
+
